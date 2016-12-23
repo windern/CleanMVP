6c8f89f0e369933bbc9bbd88ec2c501b94737009
wenxinlin <wenxinlin@guazi.com>
2016-12-23 18:40:28 +0800

    v7.dialog,progressdialog尝试

---
+++ app/src/main/java/com/windern/cleanmvp/presentation/dialog/DialogPracticeActivity.java
@@ -0,0 +1,76 @@
+package com.windern.cleanmvp.presentation.dialog;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import com.blankj.utilcode.utils.ToastUtils;
+import com.windern.cleanmvp.R;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Observable;
+import rx.functions.Action1;
+
+public class DialogPracticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_practice);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_show_dialog)
+    public void onClick() {
+        showDialog();
+    }
+
+    private ProgressDialog progressDialog;
+
+    @OnClick(R.id.btn_show_progress_dialog)
+    public void showProgressDialog() {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(DialogPracticeActivity.this);
+            progressDialog.setMessage("正在处理，请稍后……");
+            //设置进度条是否可以按退回键取消
+            progressDialog.setCancelable(false);
+            //设置点击进度对话框外的区域对话框不消失
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+        progressDialog.show();
+
+        Observable.just(1)
+                .delay(3, TimeUnit.SECONDS)
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        progressDialog.dismiss();
+                    }
+                });
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(DialogPracticeActivity.this);
+        builder.setTitle("测试标题")
+                .setMessage("hello")
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ToastUtils.showShortToast("点击取消");
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ToastUtils.showShortToast("点击确定");
+                    }
+                })
+                .create()
+                .show();
+    }
+}
